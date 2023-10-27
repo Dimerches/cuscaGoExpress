8f1a5a72ed355dfddd53b8299038abc1380843df
Dimerches <ia.merches@ufg.edu.sv>
2023-10-27 08:02:21 -0600

    Se agrega el proyecto

---
+++ CalculoSalario/Form1.Designer.cs
@@ -0,0 +1,257 @@
+﻿namespace CalculoSalario
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnguardar = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtnombre = new System.Windows.Forms.TextBox();
+            this.txtidentificacion = new System.Windows.Forms.TextBox();
+            this.txtdiario = new System.Windows.Forms.TextBox();
+            this.txtdias = new System.Windows.Forms.TextBox();
+            this.btncalcular = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtsalario = new System.Windows.Forms.TextBox();
+            this.btnnuevo = new System.Windows.Forms.Button();
+            this.btnsalir = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Open Sans", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(179, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(347, 28);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Calculo de salario trabajadores";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtdias);
+            this.groupBox1.Controls.Add(this.txtdiario);
+            this.groupBox1.Controls.Add(this.txtidentificacion);
+            this.groupBox1.Controls.Add(this.txtnombre);
+            this.groupBox1.Controls.Add(this.btnguardar);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Font = new System.Drawing.Font("Open Sans", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(57, 94);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(606, 163);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Empleado";
+            // 
+            // btnguardar
+            // 
+            this.btnguardar.BackColor = System.Drawing.Color.Aquamarine;
+            this.btnguardar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnguardar.Font = new System.Drawing.Font("Open Sans Semibold", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnguardar.ForeColor = System.Drawing.Color.DarkCyan;
+            this.btnguardar.Location = new System.Drawing.Point(432, 66);
+            this.btnguardar.Name = "btnguardar";
+            this.btnguardar.Size = new System.Drawing.Size(137, 43);
+            this.btnguardar.TabIndex = 4;
+            this.btnguardar.Text = "Guardar el registro de empleado";
+            this.btnguardar.UseVisualStyleBackColor = false;
+            this.btnguardar.Click += new System.EventHandler(this.btnguardar_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(37, 130);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(84, 15);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "Dias laborados";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(37, 96);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(133, 15);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Asignación salario diario";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(37, 66);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 15);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Identificación";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(37, 33);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 15);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Nombre";
+            // 
+            // txtnombre
+            // 
+            this.txtnombre.Location = new System.Drawing.Point(247, 30);
+            this.txtnombre.Name = "txtnombre";
+            this.txtnombre.Size = new System.Drawing.Size(87, 22);
+            this.txtnombre.TabIndex = 5;
+            // 
+            // txtidentificacion
+            // 
+            this.txtidentificacion.Location = new System.Drawing.Point(247, 63);
+            this.txtidentificacion.Name = "txtidentificacion";
+            this.txtidentificacion.Size = new System.Drawing.Size(87, 22);
+            this.txtidentificacion.TabIndex = 6;
+            // 
+            // txtdiario
+            // 
+            this.txtdiario.Location = new System.Drawing.Point(247, 93);
+            this.txtdiario.Name = "txtdiario";
+            this.txtdiario.Size = new System.Drawing.Size(87, 22);
+            this.txtdiario.TabIndex = 7;
+            // 
+            // txtdias
+            // 
+            this.txtdias.Location = new System.Drawing.Point(247, 127);
+            this.txtdias.Name = "txtdias";
+            this.txtdias.Size = new System.Drawing.Size(87, 22);
+            this.txtdias.TabIndex = 8;
+            // 
+            // btncalcular
+            // 
+            this.btncalcular.BackColor = System.Drawing.Color.NavajoWhite;
+            this.btncalcular.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btncalcular.Font = new System.Drawing.Font("Open Sans", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btncalcular.ForeColor = System.Drawing.Color.Orange;
+            this.btncalcular.Location = new System.Drawing.Point(272, 283);
+            this.btncalcular.Name = "btncalcular";
+            this.btncalcular.Size = new System.Drawing.Size(142, 49);
+            this.btncalcular.TabIndex = 2;
+            this.btncalcular.Text = "Calcular salario";
+            this.btncalcular.UseVisualStyleBackColor = false;
+            this.btncalcular.Click += new System.EventHandler(this.btncalcular_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Open Sans", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(94, 356);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(122, 15);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Valor total devengado";
+            // 
+            // txtsalario
+            // 
+            this.txtsalario.Location = new System.Drawing.Point(272, 353);
+            this.txtsalario.Name = "txtsalario";
+            this.txtsalario.Size = new System.Drawing.Size(142, 20);
+            this.txtsalario.TabIndex = 10;
+            // 
+            // btnnuevo
+            // 
+            this.btnnuevo.BackColor = System.Drawing.Color.PaleGreen;
+            this.btnnuevo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnnuevo.Font = new System.Drawing.Font("Open Sans Semibold", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnnuevo.ForeColor = System.Drawing.Color.SeaGreen;
+            this.btnnuevo.Location = new System.Drawing.Point(538, 394);
+            this.btnnuevo.Name = "btnnuevo";
+            this.btnnuevo.Size = new System.Drawing.Size(75, 23);
+            this.btnnuevo.TabIndex = 11;
+            this.btnnuevo.Text = "Nuevo";
+            this.btnnuevo.UseVisualStyleBackColor = false;
+            this.btnnuevo.Click += new System.EventHandler(this.btnnuevo_Click);
+            // 
+            // btnsalir
+            // 
+            this.btnsalir.BackColor = System.Drawing.Color.LightPink;
+            this.btnsalir.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnsalir.Font = new System.Drawing.Font("Open Sans Semibold", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnsalir.ForeColor = System.Drawing.Color.Crimson;
+            this.btnsalir.Location = new System.Drawing.Point(655, 394);
+            this.btnsalir.Name = "btnsalir";
+            this.btnsalir.Size = new System.Drawing.Size(75, 23);
+            this.btnsalir.TabIndex = 12;
+            this.btnsalir.Text = "Salir";
+            this.btnsalir.UseVisualStyleBackColor = false;
+            this.btnsalir.Click += new System.EventHandler(this.btnsalir_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnsalir);
+            this.Controls.Add(this.btnnuevo);
+            this.Controls.Add(this.txtsalario);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.btncalcular);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnguardar;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtdias;
+        private System.Windows.Forms.TextBox txtdiario;
+        private System.Windows.Forms.TextBox txtidentificacion;
+        private System.Windows.Forms.TextBox txtnombre;
+        private System.Windows.Forms.Button btncalcular;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtsalario;
+        private System.Windows.Forms.Button btnnuevo;
+        private System.Windows.Forms.Button btnsalir;
+    }
+}
+
---
+++ CalculoSalario/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculoSalario
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Empleado miEmpleado = new Empleado(); //instancia de la clase Empleado
+        Salario miSalario = new Salario(); //objeto de la clase Salario
+
+        private void btnsalir_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnguardar_Click(object sender, EventArgs e)
+        {
+            /*los valores obtenidos en los textbox son pasados a los atributos por medio de sus
+            propiedades, note que mandamos a llamar a través de los objetos creados*/
+            miEmpleado.Nombre = txtnombre.Text;
+            miEmpleado.Identificacion = txtidentificacion.Text;
+            miEmpleado.SalarioDiario = Convert.ToDecimal(txtdiario.Text);
+            miSalario.DiasLaborados = int.Parse(txtdias.Text);
+            MessageBox.Show("Datos ingresados con éxito");
+        }
+
+        private void btncalcular_Click(object sender, EventArgs e)
+        {
+            /*Enviaremos el valor de salario calculado al textbox respectivo, como es un dato
+            numérico debemos convertirlo a texto, el cálculo lo hace el método de la clase salario*/
+            txtsalario.Text = Convert.ToString(miSalario.CalcularSalario(miSalario.DiasLaborados,
+            miEmpleado.SalarioDiario));
+        }
+
+        private void btnnuevo_Click(object sender, EventArgs e)
+        {
+            //limpiando todo para ingresar nuevos datos
+            txtnombre.Clear();
+            txtidentificacion.Clear();
+            txtdias.Clear();
+            txtdiario.Clear();
+            txtsalario.Clear();
+            txtnombre.Focus(); //regresa el cursor al textbox del nombre
+        }
+
+    }
+}
